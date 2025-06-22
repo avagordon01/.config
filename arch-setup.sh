bdc1238acdd9f2ffb3465d5ec96b5606b6562183
Ava Gordon <avagordon01@gmail.com>
2025-06-22 21:39:03 +0100

    dump

--- arch-setup.sh
+++ arch-setup.sh
@@ -171,12 +171,18 @@ yay -S \
 
 #install vscode extensions
 for ext in \
+    astral-sh.ty \
     charliermarsh.ruff \
+    llvm-vs-code-extensions.vscode-clangd \
+    ms-ossdata.vscode-pgsql \
     ms-python.debugpy \
     ms-python.python \
     ms-python.vscode-pylance \
     ms-toolsai.jupyter \
+    ms-toolsai.jupyter-keymap \
     ms-toolsai.jupyter-renderers \
+    ms-toolsai.vscode-jupyter-cell-tags \
+    ms-toolsai.vscode-jupyter-slideshow \
     ms-vscode-remote.remote-ssh \
     ms-vscode-remote.remote-ssh-edit \
     ms-vscode.cpptools \
