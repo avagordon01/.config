7599d37e565e22d288bcaef05bbe0bf5aae0ff74
Ava Gordon <avagordon01@gmail.com>
2025-06-22 22:34:19 +0100

    add dev-setup script

---
+++ dev-setup.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+set -ex
+
+packages="\
+    atool \
+    bash-completion \
+    binutils \
+    bloaty \
+    bpftrace \
+    buildah \
+    clang \
+    cmake \
+    code \
+    curl \
+    fd \
+    fzf \
+    gcc \
+    gdb \
+    git \
+    github-cli \
+    htop \
+    jq \
+    lftp \
+    libqalculate \
+    lsof \
+    ltrace \
+    meson \
+    moreutils \
+    ncdu \
+    neovim \
+    nmap \
+    numactl \
+    openssh \
+    patchelf \
+    perf \
+    pkgconf \
+    podman \
+    postgresql \
+    progress \
+    python \
+    rclone \
+    ripgrep \
+    rizin \
+    rsync \
+    ruff \
+    rustup \
+    rz-cutter \
+    rz-ghidra \
+    skopeo \
+    socat \
+    strace \
+    sudo \
+    toolbox \
+    tree \
+    uv \
+    wget \
+    which \
+    wireshark-cli \
+    wl-clipboard \
+    yt-dlp \
+"
+
+source /etc/os-release
+if [ "$NAME" = "Arch Linux" ]; then
+    sudo pacman --noconfirm -Syu $packages
+
+    curl -o https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+    tar -xf yay.tar.gz
+    pushd yay
+    makepkg -si
+    popd
+
+    yay -S \
+        code-features \
+        code-marketplace
+elif [ "$NAME" = "Fedora Linux" ]; then
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+    dnf check-update
+
+    sudo dnf install --assumeyes $packages
+fi
+exit
+
+cd $HOME
+git clone https://github.com/avagordon01/.config
+cd .config
+ln -rs bash/.* ~
+
+for ext in \
+    astral-sh.ty \
+    charliermarsh.ruff \
+    llvm-vs-code-extensions.vscode-clangd \
+    ms-ossdata.vscode-pgsql \
+    ms-python.debugpy \
+    ms-python.python \
+    ms-python.vscode-pylance \
+    ms-toolsai.jupyter \
+    ms-toolsai.jupyter-keymap \
+    ms-toolsai.jupyter-renderers \
+    ms-toolsai.vscode-jupyter-cell-tags \
+    ms-toolsai.vscode-jupyter-slideshow \
+    ms-vscode-remote.remote-ssh \
+    ms-vscode-remote.remote-ssh-edit \
+    ms-vscode.cpptools \
+    ms-vscode.remote-explorer \
+    rust-lang.rust-analyzer \
+    urld.nofrills \
+    vscodevim.vim
+; do
+    code --install-extension $ext
+done
